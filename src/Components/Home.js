bc09483e38f71aeb30e1a4f8189dd11b91be582f
Tyler Bray <tylerhbray@gmail.com>
2024-01-17 15:29:32 -0800

    SCSS works

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import header from '../info/assets/home/mobile/image-header.jpg';
+import './Home.scss'
 
 const Home = () => {
   return (
     <section>
       <img src={header} alt="" />
-
+      <div className="wrapper">
+        <div className="content">Hello, SCSS!</div>
+      </div>
     </section>
   );
 }
