3ed27f8c72e711985f051a9c570f44be5bd18065
Tyler Bray <tylerhbray@gmail.com>
2024-01-17 20:12:08 -0800

    edit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,9 @@
 import { MenuIcon, ShoppingCartIcon } from '@heroicons/react/outline'; // Ensure you have @heroicons/react installed
+import './Header.scss'
 
 const Header = () => {
   return (
-    <header className="bg-black text-white flex justify-between items-center p-4">
+    <header className="header bg-black text-white flex justify-between items-center p-4">
       {/* Hamburger Menu Icon */}
       <MenuIcon className="h-6 w-6 md:hidden" />
 
