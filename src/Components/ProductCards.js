918b5eb28e3f2a8ee074fbb460f982a9f7ac5be8
Tyler Bray <tylerhbray@gmail.com>
2024-04-23 12:00:00 -0700

    productcards fully responsive

--- src/Components/ProductCards.js
+++ src/Components/ProductCards.js
@@ -3,84 +3,79 @@ import { Link } from "react-router-dom";
 
 const ProductCards = ({ setIsMenuVisible }) => {
   return (
-    <div className="product-box-container">
-      <Link to="/headphones" onClick={() => setIsMenuVisible(false)}>
-        <div className="product-box">
-          <img
-            className="mk1"
-            src={"/assets/home/mobile/mk1headphone.png"}
-            alt=""
-          />
-          <div className="product-box-title mb-3">HEADPHONES</div>
-          <div className="shop-text-container">
-            <div className="shop-text">SHOP</div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="8"
-              height="12"
-              viewBox="0 0 8 12"
-              fill="none"
-            >
-              <path
-                d="M1.32178 1L6.32178 6L1.32178 11"
-                stroke="#D87D4A"
-                stroke-width="2"
-              />
-            </svg>
-          </div>
+    <div className="px-10 flex flex-col mt-20 md:flex-row md:justify-between lg:px-36">
+      {/* <Link to="/headphones" onClick={() => setIsMenuVisible(false)}> */}
+      <div className="flex flex-col items-center px-10 py-5 bg-gray-100 rounded-lg mb-20 md:mb-0 w-full">
+        <img
+          className="w-[90px] mt-[-70px] mb-8"
+          src={"/assets/home/mobile/mk1headphone.png"}
+          alt=""
+        />
+        <div className="font-bold mb-2">HEADPHONES</div>
+        <div className="flex items-center">
+          <div className="mr-2 text-gray-500">SHOP</div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="8"
+            height="12"
+            viewBox="0 0 8 12"
+            fill="none"
+          >
+            <path
+              d="M1.32178 1L6.32178 6L1.32178 11"
+              stroke="#D87D4A"
+              stroke-width="2"
+            />
+          </svg>
         </div>
-      </Link>
-      <div className="product-box">
-        <Link to="/speakers" onClick={() => setIsMenuVisible(false)}>
-          <img
-            className="mk1"
-            src={"/assets/home/mobile/image-speaker-zx9.png"}
-            alt=""
-          />
-          <div className="product-box-title mb-3">Speakers</div>
-          <div className="shop-text-container">
-            <div className="shop-text">SHOP</div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="8"
-              height="12"
-              viewBox="0 0 8 12"
-              fill="none"
-            >
-              <path
-                d="M1.32178 1L6.32178 6L1.32178 11"
-                stroke="#D87D4A"
-                stroke-width="2"
-              />
-            </svg>
-          </div>
-        </Link>
       </div>
-      <div className="product-box">
-        <Link to="/earphones" onClick={() => setIsMenuVisible(false)}>
-          <img
-            className="mk1"
-            src={"/assets/home/mobile/earphone.png"}
-            alt=""
-          />
-          <div className="product-box-title mb-3">HEADPHONES</div>
-          <div className="shop-text-container">
-            <div className="shop-text">SHOP</div>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="8"
-              height="12"
-              viewBox="0 0 8 12"
-              fill="none"
-            >
-              <path
-                d="M1.32178 1L6.32178 6L1.32178 11"
-                stroke="#D87D4A"
-                stroke-width="2"
-              />
-            </svg>
-          </div>
-        </Link>
+      <div className="flex flex-col items-center px-10 py-5 bg-gray-100 rounded-lg mb-20 md:mb-0 w-full mx-5">
+        <img
+          className="w-[90px] mt-[-70px] mb-8"
+          src={"/assets/home/mobile/image-speaker-zx9.png"}
+          alt=""
+        />
+        <div className="font-bold mb-2">SPEAKERS</div>
+        <div className="flex items-center">
+          <div className="mr-2 text-gray-500">SHOP</div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="8"
+            height="12"
+            viewBox="0 0 8 12"
+            fill="none"
+          >
+            <path
+              d="M1.32178 1L6.32178 6L1.32178 11"
+              stroke="#D87D4A"
+              stroke-width="2"
+            />
+          </svg>
+        </div>
+      </div>
+      <div className="flex flex-col items-center px-10 py-5 bg-gray-100 rounded-lg md:mb-0 w-full">
+        <img
+          className="w-[140px] mt-[-90px] mb-8"
+          src={"/assets/home/mobile/earphone.png"}
+          alt=""
+        />
+        <div className="font-bold mb-2">EARPHONES</div>
+        <div className="flex items-center">
+          <div className="mr-2 text-gray-500">SHOP</div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="8"
+            height="12"
+            viewBox="0 0 8 12"
+            fill="none"
+          >
+            <path
+              d="M1.32178 1L6.32178 6L1.32178 11"
+              stroke="#D87D4A"
+              stroke-width="2"
+            />
+          </svg>
+        </div>
       </div>
     </div>
   );
