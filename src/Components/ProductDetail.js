81d7abd47905468dca4c0ff67682e9ed5f207d5a
Tyler Bray <tylerhbray@gmail.com>
2024-05-01 14:11:00 -0700

    more edits

--- src/Components/ProductDetail.js
+++ src/Components/ProductDetail.js
@@ -184,7 +184,7 @@ const ProductDetail = ({ addToCart }) => {
           <div className="flex flex-col items-center px-10 py-5 bg-gray-100 rounded-lg mb-20 md:mb-0 w-full">
             <img
               className="w-[90px] mt-[-70px] mb-8"
-              src={"/assets/home/mobile/mk1headphone.png"}
+              src={"assets/home/mobile/mk1headphone.png"}
               alt=""
             />
             <div className="font-bold mb-2">HEADPHONES</div>
@@ -208,7 +208,7 @@ const ProductDetail = ({ addToCart }) => {
           <div className="flex flex-col items-center px-10 py-5 bg-gray-100 rounded-lg mb-20 md:mb-0 w-full md:mx-5">
             <img
               className="w-[90px] mt-[-70px] mb-8"
-              src={"/assets/home/mobile/image-speaker-zx9.png"}
+              src={"assets/home/mobile/image-speaker-zx9.png"}
               alt=""
             />
             <div className="font-bold mb-2">SPEAKERS</div>
@@ -232,7 +232,7 @@ const ProductDetail = ({ addToCart }) => {
           <div className="flex flex-col items-center px-10 py-5 bg-gray-100 rounded-lg md:mb-0 w-full">
             <img
               className="w-[140px] mt-[-90px] mb-8"
-              src={"/assets/home/mobile/earphone.png"}
+              src={"assets/home/mobile/earphone.png"}
               alt=""
             />
             <div className="font-bold mb-2">EARPHONES</div>
